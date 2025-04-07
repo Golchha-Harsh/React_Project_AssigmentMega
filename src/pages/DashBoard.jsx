9fd12d7c4cc38d6d0f143cdd1ebd89d2bcd9619f
Golchha-Harsh <harsh.golchha@systango.com>
2025-04-07 11:37:26 +0530

    Changing in Dashboard

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -16,7 +16,7 @@ const DashboardPage = () => {
 //give isLoading isError Data error
   const { isLoading, isError, data: entries, error } = useEntries();
   //this is my anotheer custom hook and I am passing all setter function for the form field so that indirctly useAddEntry will edit states in the form
-  const { addEntry,isSubmitting, submissionError, handleSubmit } = useAddEntry(
+  const {isSubmitting, submissionError, handleSubmit } = useAddEntry(
     setName,
     setTechnology,
     setCompany,
@@ -57,106 +57,3 @@ const DashboardPage = () => {
 };
 
 export default DashboardPage;
-// // src/pages/DashBoard.js
-// import React, { useState } from "react";
-// import api from "../lib/api";
-// import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-// import NewEntryForm from "../components/dashboard/NewEntry";
-// import EntryList from "../components/dashboard/EntryList";
-// import { validateName,validateTechnology,validateCompany,validateDescription } from "../utils/Validation";
-
-// const DashboardPage = () => {
-//   const queryClient = useQueryClient();
-
-//   const { isLoading, isError, data: entries, error, refetch } = useQuery({
-//     queryKey: ["entries"],
-//     queryFn: async () => {
-//       const response = await api.get("/entries");
-//       return response.data;
-//     },
-//   });
-
-//   const [name, setName] = useState("");
-//   const [technology, setTechnology] = useState("");
-//   const [company, setCompany] = useState("");
-//   const [description, setDescription] = useState("");
-//   const [validationErrors, setValidationErrors] = useState({});
-
-//   const validateForm = () => {
-//     const errors = {};
-//     const nameError = validateName(name);
-//     if (nameError) {
-//       errors.name = nameError;
-//     }
-//     const technologyError = validateTechnology(technology);
-//     if (technologyError) {
-//       errors.technology = technologyError;
-//     }
-//     const companyError = validateCompany(company);
-//     if (companyError) {
-//       errors.company = companyError;
-//     }
-//     const descriptionError = validateDescription(description);
-//     if (descriptionError) {
-//       errors.description = descriptionError;
-//     }
-//     return errors;
-//   };
-
-//   const { mutate: addEntry, isLoading: isSubmitting, error: submissionError } = useMutation({
-//     mutationFn: async (newEntry) => {
-//       const response = await api.post("/entries", newEntry);
-//       return response.data;
-//     },
-//     onSuccess: () => {
-//       queryClient.invalidateQueries(["entries"]);
-//       setName("");
-//       setTechnology("");
-//       setCompany("");
-//       setDescription("");
-//       setValidationErrors({});
-//     },
-//   });
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     const errors = validateForm();
-//     setValidationErrors(errors);
-
-//     if (Object.keys(errors).length === 0) {
-//       const newEntry = { name, technology, company, description };
-//       addEntry(newEntry);
-//     }
-//   };
-
-//   return (
-//     <div className="p-4">
-//       <h1 className="text-2xl font-bold mb-4 text-center">
-//         Dashboard Page
-//       </h1>
-
-//       <NewEntryForm
-//         name={name}
-//         technology={technology}
-//         company={company}
-//         description={description}
-//         setName={setName}
-//         setTechnology={setTechnology}
-//         setCompany={setCompany}
-//         setDescription={setDescription}
-//         validationErrors={validationErrors}
-//         handleSubmit={handleSubmit}
-//         isSubmitting={isSubmitting}
-//         submissionError={submissionError}
-//       />
-
-//       <h2 className="text-xl font-semibold mb-2">Existing Entries</h2>
-//       <EntryList
-//         isLoading={isLoading}
-//         isError={isError}
-//         error={error}
-//         entries={entries}
-//       />
-//     </div>
-//   );
-// };
